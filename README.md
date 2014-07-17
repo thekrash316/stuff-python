e75e561e2a74ba084356f2a1dea323fdfcc3c2e9
thekrash316 <thekrash316@kjdfiuer>
2014-07-17 21:59:59 +0100

    doctest'ing example

---
+++ README.md
@@ -0,0 +1,8 @@
+stuff-python
+============
+
+
+doctest_try.py
+
+python -m doctest -v doctest_try.py
+
---
+++ doctest_try.py
@@ -0,0 +1,52 @@
+
+
+def next_prime(n):
+    '''
+    >>> next_prime(-2)
+    -1
+    >>> next_prime(-1)
+    0
+    >>> next_prime(0)
+    1
+    >>> next_prime(1)
+    2
+    >>> next_prime(2)
+    3
+    >>> next_prime(3)
+    5
+    >>> next_prime(6)
+    7
+    >>> next_prime(7)
+    11
+    >>> next_prime(25)
+    29
+    >>> next_prime(22)
+    23
+    >>> next_prime(78)
+    79
+    >>> next_prime(100)
+    -1
+    '''
+
+    first = n + 1
+
+    if first < 0 and first >= 100:
+        return -1
+
+    for x in range(first, 100):
+        if is_prime(x):
+            return x
+
+    return -1
+
+
+def is_prime(n):
+    if n >= 0 and n <= 2:
+        return True
+
+    for x in range(2, n / 2 + 1):
+        if (n % x) == 0:
+            return False
+
+    return True
+            
